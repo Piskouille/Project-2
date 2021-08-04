28def61e94712ea72c62fb26a5a18deeb93a6b82
Flow-Fly <florian.aube@gmail.com>
2021-08-04 17:51:25 +0200

    idk

--- public/javascripts/userModal.js
+++ public/javascripts/userModal.js
@@ -3,5 +3,12 @@ const userMenu = document.getElementById('user-menu')
 
 modalBtn.addEventListener('click', () => {
   userMenu.classList.toggle('active')
+  const nav = document.querySelector('.nav')
+  nav.style.zIndex = 0
+  if (nav.style.zIndex == 0) {
+    nav.style.zIndex = 20
+  } else {
+    nav.style.zIndex = 0
+  }
 })
 
