1c3f94a3851a2f39f3c7da8eb47408043952e14b
Flow-Fly <florian.aube@gmail.com>
2021-08-04 19:32:41 +0200

    Following, favorites display in modal

--- app.js
+++ app.js
@@ -71,6 +71,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth/', require('./routes/auth'));
 app.use('/admin', require('./routes/admin/restaurants'));
+app.use('/users', require('./routes/ajax/userInfos'))
 app.use('/auth/ajax', require('./routes/ajax/ajaxAuth'));
 
 // catch 404 and forward to error handler
