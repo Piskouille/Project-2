720a50622e3e5676bb675dc8b7f5bb67860d8c54
Flow-Fly <florian.aube@gmail.com>
2021-08-04 22:40:24 +0200

    some stuff

--- public/javascripts/userModal.js
+++ public/javascripts/userModal.js
@@ -4,41 +4,49 @@ const prefix = 'http://localhost:5000'
 
 modalBtn.addEventListener('click', () => {
   userMenu.classList.toggle('active')
-
+  const nav = document.querySelector('.nav')
   if (userMenu.classList.contains('active')) {
     fetchInfos()
-  }
-
-  const nav = document.querySelector('.nav')
-  nav.style.zIndex = 0
-  if (nav.style.zIndex == 0) {
     nav.style.zIndex = 20
   } else {
-    nav.style.zIndex = 0
+    const timeoutId = setTimeout(() => {
+      nav.style.zIndex = 1
+      clearTimeout(timeoutId)
+    }, 300)
+    
   }
 })
 
 async function fetchInfos() {
+  const followList = document.getElementById('following-list')
+  const favoritesList = document.getElementById('favorites-list')
+  followList.innerHTML = ''
+  favoritesList.innerHTML = ''
   const id = modalBtn.getAttribute('data-id')
   const {data} = await axios.get(prefix + '/users/' + id)
+
   const favorites = data.favorites
-  const user = data.user
-  const following = user.following
+  const following = data.user.following
+
   let length = following.length > 5 ? 5 : following.length
-  let list = document.getElementById('following-list')
   for (let i = 0; i < length; i++) {
     const li = document.createElement('li')
     li.setAttribute('data-id', following[i]._id)
     li.textContent = following[i].name
-    list.append(li)
+    followList.append(li)
   }
+  let li = document.createElement('li')
+  li.innerHTML = '<li><a href="#">More...</a></li>'
+  followList.append(li)
 
   length = favorites.length > 5 ? 5 : favorites.length
-  list = document.getElementById('favortites-list')
   for (let i = 0; i < length; i++) {
     const li = document.createElement('li')
     li.setAttribute('data-id', favorites[i]._id)
     li.textContent = favorites[i].restaurant.name
-    list.append(li)
+    favoritesList.append(li)
   }
+  li = document.createElement('li')
+  li.innerHTML = '<li><a href="#">More...</a></li>'
+  favoritesList.append(li)
 }
